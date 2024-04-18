6b1fac608245e4b652784c68cfaa6bec4cfd5758
ashwinsk24 <ashwin.kolapadath@gmail.com>
2024-04-18 15:12:03 +0530

    backend integrate

---
+++ backend/core/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+
+class React(models.Model): 
+    name = models.CharField(max_length=30) 
+    detail = models.CharField(max_length=500)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
 import NavBar from "./components/common/NavBar";
 import HeroPage from "./components/pages/hero/HeroPage";
+import axios from "axios";
+import Form from "./components/Form";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  // function componentDidMount() {
+  //   let data;
+
+  //   axios
+  //     .get("http://localhost:8000/wel/")
+  //     .then((res) => {
+  //       data = res.data;
+  //       setData(data);
+  //     })
+  //     .catch((err) => {});
+  // }
   return (
     <div className="w-full h-screen flex flex-col  bg-neutral-950 text-neutral-100">
       <NavBar />
-      <HeroPage />
+      {/* <HeroPage /> */}
+      <Form />
     </div>
   );
 }
